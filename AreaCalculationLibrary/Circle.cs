635f3e8e2feec9ee6c5150cf08e6527b71526a98
Coo1ToHate <pidexiniterid@mail.ru>
2022-02-18 00:09:21 +0700

    Добавьте файлы проекта.

---
+++ AreaCalculationLibrary/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AreaCalculationLibrary
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        /// <summary>
+        /// Конструктор круга по радиусу
+        /// </summary>
+        /// <param name="radius"></param>
+        public Circle(double radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Радиус круга должен быть положительным!");
+            }
+            this.radius = radius;
+        }
+
+        /// <summary>
+        /// Метод возвращает площадь круга
+        /// </summary>
+        /// <returns></returns>
+        public override double GetSquare()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public override string ToString()
+        {
+            return $"Круг с радиусом {radius}";
+        }
+    }
+}
---
+++ AreaCalculationLibrary/Triangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AreaCalculationLibrary
+{
+    public class Triangle : Shape
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        /// <summary>
+        /// Конструктор треугольника с тремя сторонами
+        /// </summary>
+        /// <param name="a">Сторона</param>
+        /// <param name="b">Сторона</param>
+        /// <param name="c">Сторона</param>
+        public Triangle(double a, double b, double c)
+        {
+            if (a + b < c || b + c < a || a + c < b)
+            {
+                throw new ArgumentException("Треугольник с заданными сторонами не существует!");
+            }
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentException("Стороны должны быть положительными!");
+            }
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        /// <summary>
+        /// Метод возвращает площадь треугольника по трем сторонам
+        /// </summary>
+        /// <returns></returns>
+        public override double GetSquare()
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        /// <summary>
+        /// Метод указывающий является ли треугольник прямоугольным
+        /// </summary>
+        /// <returns></returns>
+        public bool IsRight()
+        {
+            return Math.Pow(a, 2) - (Math.Pow(b, 2) + Math.Pow(c, 2)) == 0 ||
+                   Math.Pow(b, 2) - (Math.Pow(c, 2) + Math.Pow(a, 2)) == 0 ||
+                   Math.Pow(c, 2) - (Math.Pow(a, 2) + Math.Pow(b, 2)) == 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Треугольник со сторонами:\nA = {a}\nB = {b}\nC = {c}";
+        }
+    }
+}
